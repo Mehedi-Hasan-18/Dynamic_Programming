cbcb3c5d88c0354fb9e56e36463fdecd250b7131
Mehedi-Hasan-18 <mdmehedihasanroby@gmail.com>
2025-12-02 02:45:53 +0800

    complete palindrome partition

--- MCM/palindrome_partition.cpp
+++ MCM/palindrome_partition.cpp
@@ -2,6 +2,81 @@
 // Example:
 //   “aba|b|bbabb|a|b|aba” is a palindrome partitioning of “ababbbabbababa”.
 
+
+// ================================OPTIMIZED===================
+#include <bits/stdc++.h>
+using namespace std;
+int dp[1000][1000];
+
+bool isPalindrome(string s, int i, int j)
+{
+    if (i == j || i > j)
+        return true;
+    while (i < j)
+    {
+        if (s[i] == s[j])
+        {
+            i++;
+            j--;
+        }
+        else
+            return false;
+    }
+
+    return true;
+}
+
+int solve(string s, int i, int j)
+{
+    if (i >= j || isPalindrome(s, i, j))
+        return 0;
+
+    if (dp[i][j] != -1)
+        return dp[i][j];
+
+    int ans = INT_MAX;
+    for (int k = i; k < j; k++)
+    {
+
+        int left=-1, right=-1;
+        if (dp[i][k] != -1)
+            left = dp[i][k];
+        else
+        {
+            left = solve(s, i, k);
+                dp[i][k] = left;
+        }
+        if (dp[k + 1][j] != -1)
+            right = dp[k+1][j];
+        else
+        {
+            right = solve(s, k + 1, j);
+            dp[k + 1][j] = right;
+        }
+        int tempAns = 1 + left + right;
+
+        if (tempAns < ans)
+            ans = tempAns;
+    }
+    return dp[i][j] = ans;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string s;
+    cin >> s;
+    memset(dp, -1, sizeof(dp));
+
+    cout << solve(s, 0, s.size() - 1);
+
+    return 0;
+}
+
+
+
+// ===================================MOST OPTIMIZED===================================
 #include <bits/stdc++.h>
 using namespace std;
 int dp[1000][1000];
